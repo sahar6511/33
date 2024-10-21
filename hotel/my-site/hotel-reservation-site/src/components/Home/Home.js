289e99a77c77978c93d8d672debb7fb70fc6182e
sahar <you@example.com>
2024-10-21 16:02:39 +0330

    m

---
+++ hotel/my-site/hotel-reservation-site/src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import TopMenu from "../TopMenu/TopMenu";
+import Header from "../Header/Header";
+import Slider from "../Slider/Slider";
+import Footer from "../Footer/Footer";
+import AboutUs from "../AboutUs/AboutUs";
+import SectionTitle from "../SectionTitle/SectionTitle";
+import SpecialRooms from "../SpecialRooms/SpecialRooms";
+
+
+// import Space from '../Space/Space';
+// import SearchPanel from '../SearchPanel/SearchPanel';
+const Home = () => {
+  return (
+    <div>
+    <Slider/>
+    <AboutUs />
+    <SpecialRooms/>
+    <Footer />
+    </div>
+  );
+};
+
+export default Home;
